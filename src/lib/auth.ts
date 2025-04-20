ec25ec1b2a8b0f4be91548e3e643270be6e00c36
Kamil Wąsik <kamilwasik15@gmail.com>
2025-04-20 13:24:18 +0200

    add discord provider

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,4 +7,10 @@ export const auth = betterAuth({
 	database: prismaAdapter(prisma, {
 		provider: "postgresql",
 	}),
+	socialProviders: {
+		discord: {
+			clientId: process.env.DISCORD_CLIENT_ID as string,
+			clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+		},
+	},
 })
