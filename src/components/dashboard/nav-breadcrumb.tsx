ef49f0b9c948560313027106a04bcc77b4a73667
Kamil Wąsik <kamilwasik15@gmail.com>
2025-05-06 16:10:03 +0200

    add breadcrumbs

---
+++ src/components/dashboard/nav-breadcrumb.tsx
@@ -0,0 +1,44 @@
+"use client"
+import { usePathname } from "next/navigation"
+import {
+	Breadcrumb,
+	BreadcrumbList,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbSeparator,
+	BreadcrumbPage,
+} from "../ui/breadcrumb"
+import { Fragment } from "react"
+
+export default function NavBreadcrumb() {
+	const path = usePathname().split("/")
+	path.shift()
+	const pathnames: Record<string, string> = { dashboard: "Panel" }
+
+	return (
+		<Breadcrumb>
+			<BreadcrumbList>
+				{path.map((pathItem, index, array) => (
+					<Fragment key={index}>
+						{array[index + 1] != null ? (
+							<>
+								<BreadcrumbItem className="hidden capitalize md:block">
+									<BreadcrumbLink href="#">
+										{pathnames[pathItem] || pathItem}
+									</BreadcrumbLink>
+								</BreadcrumbItem>
+								<BreadcrumbSeparator className="hidden md:block" />
+							</>
+						) : (
+							<BreadcrumbItem className="hidden capitalize md:block">
+								<BreadcrumbPage>
+									{pathnames[pathItem] || pathItem}
+								</BreadcrumbPage>
+							</BreadcrumbItem>
+						)}
+					</Fragment>
+				))}
+			</BreadcrumbList>
+		</Breadcrumb>
+	)
+}
