4e29e5a7c28550f0771648d2772f4c5bf95d1839
Kamil Wąsik <kamilwasik15@gmail.com>
2025-05-11 12:53:48 +0200

    add server card

--- src/app/server/[slug]/page.tsx
+++ src/app/server/[slug]/page.tsx
@@ -1,17 +1,34 @@
 import { SiteScaffold } from "@/components/shared/scaffold/site-scaffold"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
 import { prisma } from "@/lib/prisma"
 import { notFound } from "next/navigation"
 
 export default async function ServerPage({
 	params,
 }: {
-	params: { slug: string }
+	params: Promise<{ slug: string }>
 }) {
-	const servers = await prisma.serverCache.findFirst({
-		where: { id: params.slug },
+	const server = await prisma.serverCache.findFirst({
+		where: { id: (await params).slug },
 	})
-	if (servers == null) {
+	if (server == null) {
 		notFound()
 	}
-	return <SiteScaffold>TODO</SiteScaffold>
+	return (
+		<SiteScaffold>
+			<Card>
+				<CardHeader className="flex items-center">
+					<Avatar>
+						<AvatarImage
+							src={`https://cdn.discordapp.com/icons/${server.id}/${server.icon}.png`}
+							alt="Avatar"
+						/>
+						<AvatarFallback>?</AvatarFallback>
+					</Avatar>
+					<CardTitle>{server.name}</CardTitle>
+				</CardHeader>
+			</Card>
+		</SiteScaffold>
+	)
 }
